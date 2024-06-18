636a87aa0037fda2f8bb16ea087e6c1915677e0f
Хамиди Даниял Ренатович <171016036@step.kz>
2024-06-18 21:11:19 +0500

    pobeda

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from bboard.models import Bb
+from bboard.models import Rubric
+
+
+class BbAdmin(admin.ModelAdmin):
+    list_display = ('name', 'content', 'price', 'publish', 'rubric')
+    list_display_links = ('name', 'content')
+    search_fields = ('name', 'content')
+
+admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
--- bboard/views.py
+++ bboard/views.py
@@ -3,11 +3,20 @@ from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from bboard.models import Bb
+from bboard.models import Bb, Rubric
 
 
 def index(request):
     bbs = Bb.objects.order_by('-publish')
-    context = {'bbs':bbs}
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs,'rubrics': rubrics}
 
     return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
