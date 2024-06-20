f6a654e837ab418f9a915772544440b6a96f4556
Хамиди Даниял Ренатович <171016036@step.kz>
2024-06-20 19:57:02 +0500

    pobednaya

--- bboard/urls.py
+++ bboard/urls.py
@@ -2,9 +2,10 @@
 from django.urls import path
 from bboard.views import index, by_rubric, BbCreateView
 
-app_name ='bboard'
+app_name = 'bboard'
+
 urlpatterns = [
-    path('add/', BbCreateView.as_view ,name='add'),
+    path('add/', BbCreateView.as_view(),name='add'),
     path('<int:rubric_id>/', by_rubric ,name='by_rubric'),
     path('' ,index ,name='index'),
 ]
